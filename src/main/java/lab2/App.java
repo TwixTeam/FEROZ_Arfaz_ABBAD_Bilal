99091d609b988ba03e3364d8b4d048564cc8f5f7
Bilal <bilal.abbad@isep.fr>
2017-11-06 10:31:08 +0100

    [LAB2] modification des fichiers + pom

--- src/main/java/lab2/App.java
+++ src/main/java/lab2/App.java
@@ -4,10 +4,17 @@ package lab2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		if (args.length > 0) {
+
+			int i = 0;
+			for (String argument : args) {
+				System.out.format("argument [%d]: %s %n", i++, argument);
+			}
+		}
+	}
 }
