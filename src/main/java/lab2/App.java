7975a8884d1594db149336c4ab1f0b4fb174a8be
Arfaz <aferoz@hotmail.fr>
2017-11-06 18:56:24 +0100

    [App] Add database connection + request [.properties] Add log4j + jdbc properties

--- src/main/java/lab2/App.java
+++ src/main/java/lab2/App.java
@@ -1,20 +1,67 @@
 package lab2;
 
-/**
- * Hello world!
+import org.apache.log4j.*;
+import java.sql.*;
+
+/** 
+ * JDBC Connection
  *
  */
 public class App {
 
+	protected static Logger log = Logger.getLogger(App.class);
+
+	static final String JDBC_DRIVER = ${jdbc.driver};  
+   static final String DB_URL = "jdbc:mysql://localhost/sakila";
+
+   //  Database credentials
+   static final String USER = "root";
+   static final String PASS = "";
+
 	public static void main(String[] args) {
 
 		System.out.println("Hello World!");
-		if (args.length > 0) {
+		log.info("In Main class");
+		System.out.println();
 
-			int i = 0;
-			for (String argument : args) {
-				System.out.format("argument [%d]: %s %n", i++, argument);
-			}
+		Connection conn = null;
+	   Statement stmt = null;
+	   try{
+	      //STEP 2: Register JDBC driver
+	      //Class.forName("com.mysql.jdbc.Driver");
+
+	      //STEP 3: Open a connection
+	      System.out.println("Connecting to database...");
+	      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+		  //STEP 4: Execute a query
+	      System.out.println("Creating statement...");
+	      stmt = conn.createStatement();
+	      String sql;
+	      sql = "SELECT last_name FROM actor";
+	      ResultSet rs = stmt.executeQuery(sql);
+
+	      //STEP 5: Extract data from result set
+	      while(rs.next()){
+	         //Retrieve by column name
+
+	         String last = rs.getString("last_name");
+
+	         //Display values
+	         System.out.print("ID: " + id);
+	         System.out.print(", First name: " + first);
+	         System.out.println(", Last name: " + last);
+	      }
+	      //STEP 6: Clean-up environment
+	      rs.close();
+	      stmt.close();
+	      conn.close();
+	   }catch(SQLException se){
+	      //Handle errors for JDBC
+	      se.printStackTrace();
+	   }catch(Exception e){
+	      //Handle errors for Class.forName
+	      e.printStackTrace();
 		}
 	}
 }
