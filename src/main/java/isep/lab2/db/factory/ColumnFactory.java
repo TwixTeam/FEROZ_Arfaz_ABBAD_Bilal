a0c500f923cfeee25f29236ad48314a4d195bb57
Arfaz <aferoz@hotmail.fr>
2017-11-14 16:54:39 +0100

    [ColumnFavtory] edit

--- src/main/java/isep/lab2/db/factory/ColumnFactory.java
+++ src/main/java/isep/lab2/db/factory/ColumnFactory.java
@@ -1,17 +1,30 @@
 package isep.lab2.db.factory;
 
 import isep.lab2.db.entity.DbColumn;
+import org.apache.log4j.*;
 import isep.lab2.db.entity.DbTable;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public abstract class ColumnFactory {
 
+    protected static Logger log = Logger.getLogger(ColumnFactory.class);
+
     public static void createColumn(DbTable table, ResultSet colInfo) {
         //get type from ResultSet
-        String type = colInfo.getString("")
+        String type = null;
+        try {
+            type = colInfo.getString("COLUMN_TYPE");
 
-        switch(type) {
+            switch(type) {
+                case DbColumn
+            }
+
+        } catch (SQLException se) {
+            log.error(se);
+        } catch (Exception e) {
+            log.error(e);
         }
     }
 }
